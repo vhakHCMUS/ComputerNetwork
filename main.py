8aca029037f17b23ebbbca66b9597b28e1a67616
vhakHCMUS <22127201@hcmus.student.edu.vn>
2023-10-11 08:46:22 +0700

    Server mod

--- main.py
+++ main.py
@@ -1,55 +1,43 @@
 import imaplib
 import socket
 import email
-from Feature import *
-
 from email.header import decode_header
 
-# Thay đổi giá trị này cho Gmail
 IMAP_SERVER = "imap.gmail.com"
-IMAP_PORT = 993  # Sử dụng cổng IMAPS (SSL/TLS) của Gmail
-EMAIL = "your_email@gmail.com"  # Địa chỉ email Gmail
-PASSWORD = "your_password"  # Mật khẩu email Gmail
+IMAP_PORT = 993
+EMAIL = "mmtnhom@gmail.com"
+PASSWORD = "emyeubht"
 
 def read_emails():
-    # Kết nối đến máy chủ IMAP Gmail
-    mail = imaplib.IMAP4_SSL(IMAP_SERVER, IMAP_PORT)
-    mail.login(EMAIL, PASSWORD)
-
-    # Chọn hộp thư đến
+    # Đọc email mới nhất chưa đọc trong lúc bật server
     mail.select("inbox")
 
-    # Tìm tất cả email trong hộp thư
-    result, data = mail.search(None, "ALL")
+    _, data = mail.search(None, "UNSEEN")
+    mail_ids = data[0].split()
+    
+    if not mail_ids:
+        return  # Không có email mới nào
 
-    if result == "OK":
-        email_ids = data[0].split()
-        for email_id in email_ids:
-            # Lấy email theo ID
-            result, msg_data = mail.fetch(email_id, "(RFC822)")
-            if result == "OK":
-                raw_email = msg_data[0][1]
-                msg = email.message_from_bytes(raw_email)
+    latest_email_id = int(mail_ids[-1])
 
-                # Lấy tiêu đề email
-                subject, encoding = decode_header(msg["Subject"])[0]
-                if encoding is not None:
-                    subject = subject.decode(encoding)
+    _, data = mail.fetch(str(latest_email_id), "(RFC822)")
+    raw_email = data[0][1]
+    msg = email.message_from_bytes(raw_email)
 
-                # Lấy người gửi email
-                from_, encoding = decode_header(msg.get("From"))[0]
-                if encoding is not None:
-                    from_ = from_.decode(encoding)
+    # Lấy thông tin từ, tiêu đề và nội dung email
+    from_, encoding = decode_header(msg.get("From"))[0]
+    subject, encoding = decode_header(msg["Subject"])[0]
+    subject = subject if encoding is None else subject.decode(encoding)
 
-                print("From:", from_)
-                print("Subject:", subject)
-                print("Body:")
-                for part in msg.walk():
-                    if part.get_content_type() == "text/plain":
-                        body = part.get_payload(decode=True)
-                        print(body.decode("utf-8"))
-
-    mail.logout()
+    print(f"From: {from_}")
+    print(f"Subject: {subject}")
+    
+    print("Body:")
+    for part in msg.walk():
+        if part.get_content_type() == "text/plain":
+            body = part.get_payload(decode=True)
+            print(body.decode("utf-8"))
+    print()
 
 if __name__ == "__main__":
     HOST = 'localhost'
@@ -59,15 +47,10 @@ if __name__ == "__main__":
     server.bind((HOST, PORT))
     server.listen()
 
+    mail = imaplib.IMAP4_SSL(IMAP_SERVER, IMAP_PORT)
+    mail.login(EMAIL, PASSWORD)
 
     print(f"Server is listening on {HOST}:{PORT}")
 
     while True:
-        client_socket, client_address = server.accept()
-        print(f"Accepted connection from {client_address}")
-        client_socket.send(b"Hello! This is the email server. What would you like to do?")
-        data = client_socket.recv(1024)
-        if data == b"READ_EMAILS":
-            print("Client requested to read emails.")
-            read_emails()
-        client_socket.close()
+        read_emails()
