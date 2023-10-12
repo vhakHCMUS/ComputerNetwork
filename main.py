152d4126fc0009538b8ccc1187b963372f81faa4
vhakHCMUS <22127201@hcmus.student.edu.vn>
2023-10-12 21:26:34 +0700

    Server read email

--- main.py
+++ main.py
@@ -4,5 +4,21 @@ import ListApp as app
 import ListProcess as proc
 import ScreenShot
 import Power
+import socket
 
-ScreenShot.capture()
+# Host a server to read the data from email
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 8080))
+s.listen()
+
+
+print("Server listening on port 8080...")
+
+
+# Main loop
+em = em.EmailHandler()
+while True:
+    emailData = em.read_emails()
+
+    order = emailData[1]
+    print(order)
