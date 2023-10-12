31f018125fd03ea7d89d2aac42a71440048e8b97
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-10-12 20:04:43 +0700

    include stop & open application in ListApp.py

--- ListApp.py
+++ ListApp.py
@@ -1,11 +1,20 @@
 import subprocess
+import os
 
 def ListApp():
-    cmd = 'powershell "gps | where {$_.MainWindowTitle } | select Description'
+    cmd = 'powershell "gps | where {$_.MainWindowTitle } | select Description,Id,Path'
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-    listOfRunningApp = []
+    ans = []
     for line in proc.stdout:
         if not line.decode()[0].isspace():
-            listOfRunningApp.append(line.decode().rstrip())
-    listOfRunningApp.append("end")
-    return listOfRunningApp
+            ans.append(line.decode().rstrip())
+    ans.append("end")
+    return ans
+
+def OpenApp(path):
+    # eg path: C:\Users\Admin\Desktop\ComputerNetwork\app_name.exe
+    os.startfile(path)
+
+def StopApp(executeFile):
+    # eg executeFile: app_name.exe
+    subprocess.call(["taskkill","/F","/IM", executeFile])
